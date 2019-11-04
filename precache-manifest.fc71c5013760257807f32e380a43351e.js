4eedf680d26a97028ff35e34ed2dd066d7ed753f
YuraBobeshko <gorobech55@gmail.com>
2019-11-04 20:40:30 +0200

    Updates

---
+++ precache-manifest.fc71c5013760257807f32e380a43351e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c567a565b50f427392e6cb22886a878",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "48f135ade0d836805b80",
+    "url": "/react_add-todo-form/static/css/main.ec9505b9.chunk.css"
+  },
+  {
+    "revision": "11ff868d0a8a576262f7",
+    "url": "/react_add-todo-form/static/js/2.0612fd57.chunk.js"
+  },
+  {
+    "revision": "48f135ade0d836805b80",
+    "url": "/react_add-todo-form/static/js/main.aed8e5e1.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
